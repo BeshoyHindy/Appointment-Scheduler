2d4261c564b8253e9440260bb4c9dfa6b58c3449
Beshoy Hindy <Beshoy.Hindy@gmail.com>
2021-05-24 17:36:11 +0200

    initializing and constructing the solution structure

---
+++ src/Scheduler/Scheduler.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Scheduler.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Scheduler/Scheduler.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Scheduler.Domain
+{
+    public class Class1
+    {
+    }
+}
